3732932ba29057fde6047cdc437f95dc59b814d0
Leticia Fernandes <leticiamara.fernandes@gmail.com>
2015-01-31 16:13:59 -0300

    Added plugin information.

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redrisk do
   name 'Redrisk plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Role Software'
+  description 'This is a risk plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/rolesoft/redrisk'
+  author_url 'http://rolesoft.com.br'
 end
