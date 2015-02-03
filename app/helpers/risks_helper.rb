5890c7cd93fb8b0fdf2a4fa39ffa3db89d6d7155
Rodrigo Girão <rodrigogirao7@gmail.com>
2015-02-02 21:24:57 -0300

    Added select options for risk status

--- app/helpers/risks_helper.rb
+++ app/helpers/risks_helper.rb
@@ -1,2 +1,13 @@
 module RisksHelper
+
+  def options_for_status
+    [[t('risk.status.open'), 0],
+     [t('risk.status.closed'),	1],
+     [t('risk.status.in_progress'), 2],
+     [t('risk.status.suspended'), 3],
+     [t('risk.status.canceled'), 4],
+     [t('risk.status.rejected'), 5],
+     [t('risk.status.resolved'), 6]]
+  end
+
 end
--- app/models/risk.rb
+++ app/models/risk.rb
@@ -1,2 +1,33 @@
 class Risk < ActiveRecord::Base
+
+  #STATUS
+  OPEN = 0
+  CLOSED = 1
+  IN_PROGRESS = 2
+  SUSPENDED = 3
+  CANCELED = 4
+  REJECTED = 5
+  RESOLVED = 6
+
+  def to_status
+    case status
+      when OPEN
+        I18n.t('risk.status.open')
+      when CLOSED
+        I18n.t('risk.status.closed')
+      when IN_PROGRESS
+        I18n.t('risk.status.in_progress')
+      when SUSPENDED
+        I18n.t('risk.status.suspended')
+      when CANCELED
+        I18n.t('risk.status.canceled')
+      when REJECTED
+        I18n.t('risk.status.rejected')
+      when RESOLVED
+        I18n.t('risk.status.resolved')
+      else
+        'error'
+    end
+  end
+
 end
