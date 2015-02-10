883721a34ebcd9a75f334872dbf515a5a52c9a96
Rodrigo Girão <rodrigogirao7@gmail.com>
2015-02-09 22:09:49 -0300

    Change to static attributes in risk helper

--- app/helpers/risks_helper.rb
+++ app/helpers/risks_helper.rb
@@ -1,8 +1,8 @@
 module RisksHelper
 
   def options_for_origin
-    [[t('risk.origin.project'), 0],
-     [t('risk.origin.organizational'),	1]]
+    [[t('risk.origin.project'), Risk::PROJECT],
+     [t('risk.origin.organizational'),	Risk::ORGANIZATIONAL]]
   end
 
   def options_for_source
@@ -15,20 +15,20 @@ module RisksHelper
     ]
   end
   def options_for_status
-    [[t('risk.status.open'), 0],
-     [t('risk.status.closed'),	1],
-     [t('risk.status.in_progress'), 2],
-     [t('risk.status.suspended'), 3],
-     [t('risk.status.canceled'), 4],
-     [t('risk.status.rejected'), 5],
-     [t('risk.status.resolved'), 6]]
+    [[t('risk.status.open'), Risk::OPEN],
+     [t('risk.status.closed'),	Risk::CLOSED],
+     [t('risk.status.in_progress'), Risk::IN_PROGRESS],
+     [t('risk.status.suspended'), Risk::SUSPENDED],
+     [t('risk.status.canceled'), Risk::CANCELED],
+     [t('risk.status.rejected'), Risk::REJECTED],
+     [t('risk.status.resolved'), Risk::RESOLVED]]
   end
 
   def options_for_strategy
-    [[t('risk.strategy.eliminate'), 0],
-     [t('risk.strategy.transfer'),	1],
-     [t('risk.strategy.mitigate'), 2],
-     [t('risk.strategy.accept'), 3]]
+    [[t('risk.strategy.eliminate'), Risk::ELIMINATE],
+     [t('risk.strategy.transfer'),	Risk::TRANSFER],
+     [t('risk.strategy.mitigate'), Risk::MITIGATE],
+     [t('risk.strategy.accept'), Risk::ACCEPT]]
   end
 
 end
