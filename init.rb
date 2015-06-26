a10e785e5fc06ffd681f89b2ee60bb9358737ed5
Rodrigo Girão <rodrigogirao7@gmail.com>
2015-06-25 21:28:32 -0300

    Updated to version 1.0.1

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redrisk do
   name 'Redrisk plugin'
   author 'Role Software'
   description 'This is a risk plugin for Redmine'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/rolesoft/redrisk'
   author_url 'http://rolesoft.com.br'
 
