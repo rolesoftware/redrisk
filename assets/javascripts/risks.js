dc35633cd1ec5fb65ba75715584638b3cf5ec4a7
Andrey Yumashev <andrey.jumashev@gmail.com>
2015-09-10 21:32:19 +0300

    Added RU localization, added some links to show, Redmine users names are now links. Changed logic of money input - now currency (suffix or prefix) could be changed at locale

--- assets/javascripts/risks.js
+++ assets/javascripts/risks.js
@@ -34,5 +34,5 @@ function get_priority_by_probability_and_impact(url){
 }
 
 $(function() {
-    $(".money_field").maskMoney({prefix: '$ ', thousands: '.', decimal: ',', affixesStay: true});
+    $(".money_field").maskMoney({suffix: $('#money_suffix').val(), prefix: $('#money_prefix').val(), thousands: '.', decimal: ',', affixesStay: true});
 });
